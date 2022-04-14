a2305627cd4a8f351a4f0b6e2eb935beac6f96db
Tanya-11 <tanya.gupta@sjsu.edu>
2022-04-14 02:28:36 -0700

    actions with product_stock

--- actions/actions.py
+++ actions/actions.py
@@ -7,22 +7,95 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#from rasa_sdk.interfaces import Action
+from typing import Any, Text, Dict, List
 
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from rasa_sdk import Action, Tracker, FormValidationAction
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+from rasa_sdk.events import FollowupAction
+from rasa_sdk.events import BotUttered
+from rasa_sdk.types import DomainDict
+
+
+import sqlite3
+
+path_to_db = "actions/bot.db"
+
+class ActionProductSearch(Action):
+    def name(self) -> Text:
+        return "action_product_search"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        # connect to DB
+        connection = sqlite3.connect(path_to_db)
+        cursor = connection.cursor()
+
+        # get slots and save as tuple
+        shoe = [(tracker.get_slot("color")), (tracker.get_slot("size"))]
+
+        # place cursor on correct row based on search criteria
+        cursor.execute("SELECT * FROM inventory WHERE color=? AND size=?", shoe)
+        
+        # retrieve sqlite row
+        data_row = cursor.fetchone()
+
+        if data_row:
+            # provide in stock message
+            print("exists")
+            dispatcher.utter_message(text="You're in luck! We have those in stock.")
+            connection.close()
+            slots_to_reset = ["size", "color"]
+            return [SlotSet(slot, None) for slot in slots_to_reset]
+        else:
+            # provide out of stock
+            dispatcher.utter_message(text="Sorry, seems like we don't have those shoes.")
+            connection.close()
+            slots_to_reset = ["size", "color"]
+            return [SlotSet(slot, None) for slot in slots_to_reset]
+
+def clean_color(color):
+    return "".join([c for c in color if c.isalpha()])
+def clean_size(size):
+    return size.isnumeric()
+
+class ValidateNameForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_product_stock_form"
+
+    def validate_color(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `color."""
+
+        # If the name is super short, it might be wrong.
+        color = clean_color(slot_value)
+        if len(color) == 0:
+            dispatcher.utter_message(text="That must've been a typo.")
+            return {"color": None}
+        return {"color": color}
+
+    def validate_size(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate `size` value."""
+
+        # If the name is super short, it might be wrong.
+        size = clean_size(slot_value)
+        if len(size) == 0:
+            dispatcher.utter_message(text="That must've been a typo.")
+            return {"size": None}
+        return {"size": size}
